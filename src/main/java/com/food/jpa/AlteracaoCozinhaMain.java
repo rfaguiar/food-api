012ba2faadeb3af56054919b43f54e44a0602177
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-08-29 20:47:59 -0300

    Alterar objeto por id do banco de dados com classe de teste usando spring application context

---
+++ src/main/java/com/food/jpa/AlteracaoCozinhaMain.java
@@ -0,0 +1,22 @@
+package com.food.jpa;
+
+import com.food.Application;
+import com.food.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(Application.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        Cozinha cozinhaBr = new Cozinha();
+        cozinhaBr.setId(1L);
+        cozinhaBr.setNome("Brasileira");
+        System.out.println(cadastroCozinha.adicionar(cozinhaBr));
+    }
+}
