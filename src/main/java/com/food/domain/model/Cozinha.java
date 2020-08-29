7be4bda4ea1d5de75c9fd72ba28593f2ccb45b5d
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-08-29 15:49:43 -0300

    Consultando objetos do banco de dados com classe de teste usando spring application context

--- src/main/java/com/food/domain/model/Cozinha.java
+++ src/main/java/com/food/domain/model/Cozinha.java
@@ -42,4 +42,12 @@ public class Cozinha {
     public int hashCode() {
         return Objects.hash(id);
     }
+
+    @Override
+    public String toString() {
+        return "Cozinha{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
+    }
 }
