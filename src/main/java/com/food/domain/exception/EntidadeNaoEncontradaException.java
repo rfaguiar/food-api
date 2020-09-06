e08b3daa4191805368e34d7ab87ae56bfbb727e2
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-05 22:39:28 -0300

    Lançando exceções customizadas anotadas com @ResponseStatus

--- src/main/java/com/food/domain/exception/EntidadeNaoEncontradaException.java
+++ src/main/java/com/food/domain/exception/EntidadeNaoEncontradaException.java
@@ -1,5 +1,9 @@
 package com.food.domain.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class EntidadeNaoEncontradaException extends RuntimeException {
     public EntidadeNaoEncontradaException(String message) {
         super(message);
