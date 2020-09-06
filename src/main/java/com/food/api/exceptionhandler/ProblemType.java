c1c099c58fc264351e5fc4cd173bb159c1c4f98a
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-06 19:27:19 -0300

    Customizando exception handlers de ResponseEntityExceptionHandler

--- src/main/java/com/food/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/food/api/exceptionhandler/ProblemType.java
@@ -2,6 +2,7 @@ package com.food.api.exceptionhandler;
 
 public enum ProblemType {
 
+    MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem incompreensível"),
     ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio");
