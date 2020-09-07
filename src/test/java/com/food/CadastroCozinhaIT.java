c855d764156181681c90b8839b1f414895e80dda
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-07 20:23:15 -0300

    Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/food/CadastroCozinhaIT.java
+++ src/test/java/com/food/CadastroCozinhaIT.java
@@ -2,8 +2,10 @@ package com.food;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -18,11 +20,15 @@ class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @BeforeEach
     public void begin() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+        flyway.migrate();
     }
 
     @Test
@@ -47,4 +53,15 @@ class CadastroCozinhaIT {
             .body("nome", hasItems("Indiana", "Tailandesa"));
     }
 
+    @Test
+    void deveRetornarStatus201QuandoCadastrarCozinha() {
+        given()
+            .body("{\"nome\": \"Chinesa\"}")
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.CREATED.value());
+    }
 }
