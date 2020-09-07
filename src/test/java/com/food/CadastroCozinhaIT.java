45b7e9ffe7038bc655c04607ce1a857465bb1752
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-07 19:51:39 -0300

    Configurando Maven Failsafe Plugin no projeto

--- src/test/java/com/food/CadastroCozinhaIT.java
+++ src/test/java/com/food/CadastroCozinhaIT.java
@@ -13,7 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @SpringBootTest
-class CadastroCozinhaIntegrationTests {
+class CadastroCozinhaIT {
 
     @Autowired
     private CozinhaService cozinhaService;
