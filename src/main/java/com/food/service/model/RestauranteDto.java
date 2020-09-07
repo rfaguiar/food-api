ece7391b0a6e2d63c9e9063d1575a385e2c91c3a
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-07 09:00:05 -0300

    Conhecendo e adicionando mais constraints de validação no modelo

--- src/main/java/com/food/service/model/RestauranteDto.java
+++ src/main/java/com/food/service/model/RestauranteDto.java
@@ -3,18 +3,26 @@ package com.food.service.model;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.food.domain.model.Restaurante;
 
-import javax.validation.constraints.NotNull;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
 import java.math.BigDecimal;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-public record RestauranteDto(@JsonProperty("id") Long id,
-                             @JsonProperty("nome") @NotNull String nome,
-                             @JsonProperty("taxaFrete") BigDecimal taxaFrete,
-                             @JsonProperty("cozinha") CozinhaDto cozinha,
-                             @JsonProperty("formasPagamento") List<FormaPagamentoDto> formasPagamento) {
+public record RestauranteDto(@JsonProperty("id")
+                             Long id,
+                             @JsonProperty("nome")
+                             @NotBlank
+                             String nome,
+                             @JsonProperty("taxaFrete")
+                             @PositiveOrZero
+                             BigDecimal taxaFrete,
+                             @JsonProperty("cozinha")
+                             CozinhaDto cozinha,
+                             @JsonProperty("formasPagamento")
+                             List<FormaPagamentoDto> formasPagamento) {
     
     public RestauranteDto (Restaurante restaurante) {
         this(restaurante.id(), restaurante.nome(), restaurante.taxaFrete(),
