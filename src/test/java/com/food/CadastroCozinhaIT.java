f4e65e756449364786f7112f5951d4aa5d9c1d9b
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-07 21:58:46 -0300

    implementado mais testes de endpoints de cozinha

--- src/test/java/com/food/CadastroCozinhaIT.java
+++ src/test/java/com/food/CadastroCozinhaIT.java
@@ -5,6 +5,8 @@ import com.food.domain.repository.CozinhaRepository;
 import com.food.util.DatabaseCleaner;
 import com.food.util.ResourceUtils;
 import io.restassured.RestAssured;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -39,7 +41,7 @@ class CadastroCozinhaIT {
 
     @BeforeEach
     public void begin() {
-        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        RestAssured.filters(new RequestLoggingFilter(), new ResponseLoggingFilter());
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
         databaseCleaner.clearTables();
@@ -66,7 +68,7 @@ class CadastroCozinhaIT {
     }
 
     @Test
-    void deveRetornarConter4CozinhasQuandoConsultarCozinhas() {
+    void deveRetornarConterCozinhasQuandoConsultarCozinhas() {
         given()
             .accept(ContentType.JSON)
         .when()
@@ -110,5 +112,67 @@ class CadastroCozinhaIT {
             .post()
         .then()
             .statusCode(HttpStatus.CREATED.value());
+    }
+
+    @Test
+    void deveRetornarStatus400QuandoCadastrarCozinhaComNomeNulo() {
+        String jsonCozinhaNomeNulo = ResourceUtils.getContentFromResource(
+                "/json/correto/cozinha-nome-null.json");
+        given()
+            .body(jsonCozinhaNomeNulo)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .post()
+        .then()
+            .statusCode(HttpStatus.BAD_REQUEST.value());
+    }
+
+    @Test
+    void deveRetornarStatus204QuandoRemoverCozinha() {
+        given()
+            .pathParam("cozinhaId", cozinhaAmericana.id())
+            .accept(ContentType.JSON)
+        .when()
+            .delete("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.NO_CONTENT.value());
+    }
+
+    @Test
+    void deveRetornarStatus404QuandoRemoverCozinhaInexistente() {
+        given()
+            .pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
+            .accept(ContentType.JSON)
+        .when()
+            .delete("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    void deveRetornarStatus404QuandoAtualizarCozinhaInexistente() {
+        given()
+            .pathParam("cozinhaId", COZINHA_ID_INEXISTENTE)
+            .body(jsonCorretoCozinhaChinesa)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .put("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.NOT_FOUND.value());
+    }
+
+    @Test
+    void deveRetornarStatus200QuandoAtualizarCozinha() {
+        given()
+            .pathParam("cozinhaId", cozinhaAmericana.id())
+            .body(jsonCorretoCozinhaChinesa)
+            .contentType(ContentType.JSON)
+            .accept(ContentType.JSON)
+        .when()
+            .put("/{cozinhaId}")
+        .then()
+            .statusCode(HttpStatus.OK.value());
     }
 }
