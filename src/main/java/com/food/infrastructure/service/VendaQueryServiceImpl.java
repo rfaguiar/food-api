b7959c1baa5961df08c2aaca6797ec9dc805b849
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-24 20:59:36 -0300

    adicionado os filtros na consulta de vendas diárias

--- src/main/java/com/food/infrastructure/service/VendaQueryServiceImpl.java
+++ src/main/java/com/food/infrastructure/service/VendaQueryServiceImpl.java
@@ -3,11 +3,14 @@ package com.food.infrastructure.service;
 import com.food.api.model.dto.VendaDiaria;
 import com.food.domain.filter.VendaDiariaFilter;
 import com.food.domain.model.Pedido;
+import com.food.domain.model.StatusPedido;
 import com.food.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
@@ -23,6 +26,24 @@ public class VendaQueryServiceImpl implements VendaQueryService {
         var builder = entityManager.getCriteriaBuilder();
         var query = builder.createQuery(VendaDiaria.class);
         var root = query.from(Pedido.class);
+        var predicates = new ArrayList<Predicate>();
+
+        if (filtro.restauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.restauranteId()));
+        }
+
+        if (filtro.dataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.dataCriacaoInicio()));
+        }
+
+        if (filtro.dataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.dataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
 
         var functionDateDataCriacao= builder.function(
                 "date", Date.class, root.get("dataCriacao"));
@@ -34,6 +55,7 @@ public class VendaQueryServiceImpl implements VendaQueryService {
                 builder.sum(root.get("valorTotal")));
         query.select(selection);
         query.groupBy(functionDateDataCriacao);
+        query.where(predicates.toArray(Predicate[]::new));
 
         return entityManager.createQuery(query).getResultList();
     }
