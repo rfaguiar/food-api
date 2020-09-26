85b0529eac2afba26c5fd7ad6a5c778b6e36ae3a
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-26 14:19:24 -0300

    Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/food/domain/model/FotoProduto.java
@@ -0,0 +1,25 @@
+package com.food.domain.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.MapsId;
+import javax.persistence.OneToOne;
+
+@Entity
+public record FotoProduto(@Id
+                          @Column(name = "produto_id")
+                          Long id,
+                          @OneToOne(fetch = FetchType.LAZY)
+                          @MapsId
+                          Produto produto,
+                          String nomeArquivo,
+                          String descricao,
+                          String contentType,
+                          Long tamanho) {
+
+    public FotoProduto() {
+        this(null, null, null, null, null, null);
+    }
+}
