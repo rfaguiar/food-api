8a5f8d3c86fef04633f77568c2d4673b27763e81
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-09-27 11:43:50 -0300

    Adicionando o SDK Java da Amazon S3 no projeto e criando classe do serviço de storage

---
+++ src/main/java/com/food/infrastructure/service/S3FotoStorageServiceImpl.java
@@ -0,0 +1,25 @@
+package com.food.infrastructure.service;
+
+import com.food.service.FotoStorageService;
+import org.springframework.stereotype.Service;
+
+import java.io.InputStream;
+
+@Service
+public class S3FotoStorageServiceImpl implements FotoStorageService {
+
+    @Override
+    public void armazenar(NovaFoto novaFoto) {
+
+    }
+
+    @Override
+    public void remover(String nomeArquivo) {
+
+    }
+
+    @Override
+    public InputStream recuperar(String nomeArquivo) {
+        return null;
+    }
+}
