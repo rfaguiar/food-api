96ea4016866088bc77a703c4c9cb237f737aba81
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-03 10:33:32 -0300

    Implementado uma chamada na REST API com JavaScript. Habilitado CORS em controladores e métodos com @CrossOrigin

---
+++ client-html-js/client.js
@@ -0,0 +1,11 @@
+function consultarRestaurantes() {
+    $.ajax({
+        url: "http://localhost:8080/restaurantes",
+        type: "get",
+        success: function(response) {
+            $("#conteudo").text(JSON.stringify(response));
+        }
+    });
+}
+
+$("#botao").click(consultarRestaurantes);
