cc53a86aed4f92eb50dd88a4457803623976cedb
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-03 10:49:26 -0300

    Habilitado CORS globalmente no projeto da API

--- src/main/java/com/food/api/controller/RestauranteController.java
+++ src/main/java/com/food/api/controller/RestauranteController.java
@@ -5,7 +5,6 @@ import com.food.api.model.response.RestauranteResponse;
 import com.food.service.RestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -22,7 +21,6 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin
 @RestController
 @RequestMapping("/restaurantes")
 public class RestauranteController {
