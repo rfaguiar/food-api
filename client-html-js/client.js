2e7ecd9ab112cae52569a7dcdfe05d7c516fc8a6
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-03 10:52:58 -0300

    implementado uma requisição GET com JavaScript

--- client-html-js/client.js
+++ client-html-js/client.js
@@ -10,7 +10,7 @@ function consultarRestaurantes() {
 
 function fecharRestaurante() {
     $.ajax({
-        url: "http://api.algafood.local:8080/restaurantes/1/fechamento",
+        url: "http://localhost:8080/restaurantes/1/fechamento",
         type: "put",
 
         success: function(response) {
@@ -19,4 +19,31 @@ function fecharRestaurante() {
     });
 }
 
-$("#botao").click(consultarRestaurantes);
+function consultar() {
+    $.ajax({
+        url: "http://localhost:8080/formas-pagamento",
+        type: "get",
+
+        success: function(response) {
+            preencherTabela(response);
+        }
+    });
+}
+
+function preencherTabela(formasPagamento) {
+    $("#tabela tbody tr").remove();
+
+    $.each(formasPagamento, function(i, formaPagamento) {
+        var linha = $("<tr>");
+
+        linha.append(
+            $("<td>").text(formaPagamento.id),
+            $("<td>").text(formaPagamento.descricao)
+        );
+
+        linha.appendTo("#tabela");
+    });
+}
+
+
+$("#btn-consultar").click(consultar);
