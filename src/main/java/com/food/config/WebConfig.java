6067b32af0b2a83322a56dc2b6eb2941de06742f
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-12 19:58:56 -0300

    Definindo a versão padrão da API quando o Media Type não é informado

--- src/main/java/com/food/config/WebConfig.java
+++ src/main/java/com/food/config/WebConfig.java
@@ -1,8 +1,11 @@
 package com.food.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import static com.food.config.FoodMediaTypes.V2_APPLICATION_JSON;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
@@ -11,5 +14,10 @@ public class WebConfig implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
         .allowedMethods("*");
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.defaultContentType(V2_APPLICATION_JSON);
     }
 }
