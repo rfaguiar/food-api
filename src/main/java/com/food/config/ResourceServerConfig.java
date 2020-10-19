54a9cf67276d6048562adc1358dd5078d69b8180
Rogerio Francisco de Aguiar <rfaguiar1@gmail.com>
2020-10-18 21:17:04 -0300

    Carregando as Granted Authorities e restringindo acesso a endpoints na API

--- src/main/java/com/food/config/ResourceServerConfig.java
+++ src/main/java/com/food/config/ResourceServerConfig.java
@@ -1,14 +1,14 @@
 package com.food.config;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.oauth2.jwt.JwtDecoder;
-import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
 
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
+import java.util.Collections;
+import java.util.stream.Collectors;
 
 @EnableWebSecurity
 public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
@@ -17,14 +17,32 @@ public class ResourceServerConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .anyRequest().authenticated()
+                .antMatchers(HttpMethod.POST, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+                .antMatchers(HttpMethod.PUT, "/v1/cozinhas/**").hasAuthority("EDITAR_COZINHAS")
+                .antMatchers(HttpMethod.GET, "/v1/cozinhas/**").authenticated()
+                .anyRequest().denyAll()
             .and()
             .cors()
             .and()
             .oauth2ResourceServer()
             .jwt()
+            .jwtAuthenticationConverter(jwtAuthenticationConverter())
 //                .opaqueToken()
         ;
+    }
+
+    private JwtAuthenticationConverter jwtAuthenticationConverter() {
+        var jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(jwt -> {
+            var authorities = jwt.getClaimAsStringList("authorities");
+            if (authorities == null) {
+                authorities = Collections.emptyList();
+            }
+            return authorities.stream()
+                    .map(SimpleGrantedAuthority::new)
+                    .collect(Collectors.toList());
+        });
+        return jwtAuthenticationConverter;
     }
 /*
     Config Chave simetrica
